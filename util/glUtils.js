c01ea6d0db56444c278ac6ffedab497634722f53
Pichlerdom <pichlerdominik97@gmail.com>
2018-06-21 16:04:12 +0200

    Add files via upload

--- util/glUtils.js
+++ util/glUtils.js
@@ -1,42 +1,15 @@
-/**
-* This file contains various functions / objects / classes that are useful for
-* working with opengl
-**/
 
-/**
-* Function that binds a buffer to an attribute in the currently bound shader program
-* and activates it.
-* The function assumes that content is tightly packed (stride = 0 & offset = 0)
-* @param buffer - buffer to bind
-* @param bufferLoc - attribute location to bind buffer to
-* @param numElems - number of elements in the buffer (a buffer with triangle data
-*                    would have #floats/3 as the number of elements)
-**/
 function setArrayBufferFloat(buffer, bufferLoc, numElems) {
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
     gl.vertexAttribPointer(bufferLoc, numElems, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(bufferLoc);
 }
 
-/**
-* Similar to setArrayBufferFloat but sets the attribute divisor of the buffer being set
-* @param buffer - buffer to bind
-* @param bufferLoc - attribute location to bind buffer to
-* @param numElems - number of elements in the buffer (a buffer with triangle data
-*                    would have #floats/3 as the number of elements)
-* @param divisor - attribute divisor of the buffer (0 -> same for all instances; 1 -> different for every instance)
-**/
-function setArrayBufferFloatInstanced(buffer, bufferLoc, numElems, divisor) {
+function setArrayBufferFloatInstanced(buffer, bufferLoc, numElems, numInstances) {
     setArrayBufferFloat(buffer, bufferLoc, numElems);
-    gl.vertexAttribDivisor(bufferLoc, divisor);
+    gl.vertexAttribDivisor(bufferLoc, numInstances);
 }
 
-/**
-* Sets up an array buffer whose data doesn't change. Creates a buffer, writes the data
-* and returns the bufferindex
-* @param bufferData - data to be written into the buffer
-* @returns bufferindex of the created buffer
-**/
 function setupStaticArrayBuffer(bufferData) {
     var buffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
@@ -44,11 +17,6 @@ function setupStaticArrayBuffer(bufferData) {
     return buffer;
 }
 
-/**
-* Similar to setupStaticArrayBuffer but creates an ELEMENT_ARRAY_BUFFER instead
-* @param bufferData - data to be written into the buffer
-* @returns bufferindex of the created buffer
-**/
 function setUpStaticElementBuffer(bufferData) {
     buffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffer);
@@ -56,31 +24,62 @@ function setUpStaticElementBuffer(bufferData) {
     return buffer;
 }
 
-/**
-* Write data into static array buffer. Sets buffer data with STATIC_DRAW flag
-* @param buffer - buffer to write data into
-* @param data - data to write
-**/
 function setStaticArrayBufferData(buffer, data) {
 	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
 	gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
 }
 
-/**
-* Write data into dynamic array buffer. Sets buffer data with DYNAMIC_DRAW flag
-* @param buffer - buffer to write data into
-* @param data - data to write
-**/
 function setDynamicArrayBufferData(buffer, data) {
 	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
 	gl.bufferData(gl.ARRAY_BUFFER, data, gl.DYNAMIC_DRAW);
 }
 
+//shader class
+function ShaderProgram(vs, fs, varyings) {
+    this.program = createTransformFeedbackProgram(gl, vs, fs, varyings);
+
+    this.modelViewLoc = gl.getUniformLocation(this.program, 'u_modelView');
+    this.projectionLocation = gl.getUniformLocation(this.program, 'u_projection');
+    this.positionLocation = gl.getAttribLocation(this.program, "a_position");
+
+    this.setProjectionMat = function(projectionMatrix) {
+        gl.uniformMatrix4fv(this.projectionLocation, false, projectionMatrix);
+    };
+
+    this.setupModelView = function(viewMatrix, sceneMatrix) {
+        var modelViewMatrix = [];
+        mat4.mul(modelViewMatrix, viewMatrix, sceneMatrix);
+        gl.uniformMatrix4fv(this.modelViewLoc, false, modelViewMatrix);
+    }
+}
+
 /**
-* Create WebGL2 context. This is actually pretty much identical to the createWebGlContext
-* function in the lab framework. A webgl2 context is needed for the particle effects
-* as they are drawn using instanced rendering, a feature not present in webgl1
-**/
+ * creates a program by the given vertex and fragment shader
+ * @param gl GL context
+ * @param vertex vertex shader or code
+ * @param fragment fragment shader or code
+ * @returns {WebGLProgram}
+ */
+function createTransformFeedbackProgram(gl, vertex, fragment, varyings) {
+  var program = gl.createProgram();
+  gl.attachShader(program, typeof vertex === 'string' ? createShader(gl, vertex, gl.VERTEX_SHADER) : vertex);
+  gl.attachShader(program, typeof fragment === 'string' ? createShader(gl, fragment, gl.FRAGMENT_SHADER) : fragment);
+  if(varyings != null) {
+      gl.transformFeedbackVaryings(program, varyings, gl.SEPARATE_ATTRIBS);
+  }
+  gl.linkProgram(program);
+  var linked = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (!linked) {
+    // something went wrong with the link
+    var lastError = gl.getProgramInfoLog(program);
+    console.error('Error in program linking:' + lastError);
+    gl.deleteProgram(program);
+    return null;
+  }
+  return program;
+}
+
+
 function createWebGL2Context(width, height) {
   var canvas = document.createElement('canvas');
   canvas.width = width || 400;
@@ -92,7 +91,7 @@ function createWebGL2Context(width, height) {
 
 
 //TODO: ADD TEXTURES
-//convenience function for drawing a unit cube.
+//convenience function for drawing a unit cube with specified color
 function cubeRenderer() {
     let s = 0.5;
 
@@ -114,7 +113,7 @@ function cubeRenderer() {
         0,1,0, 0,1,0, 0,1,0, 0,1,0
     ])
 
-    var cubeIndices =  new Uint16Array([
+    var cubeIndices =  new Float32Array([
        0,1,2, 0,2,3,
        4,5,6, 4,6,7,
        8,9,10, 8,10,11,
@@ -123,34 +122,16 @@ function cubeRenderer() {
        20,21,22, 20,22,23
     ]);
 
-    var cubeTextureCoords = new Float32Array([
-      0,0, 0,1, 1,1, 1,0,
-      0,0, 0,1, 1,1, 1,0,
-      0,0, 0,1, 1,1, 1,0,
-      0,0, 0,1, 1,1, 1,0,
-      0,0, 0,1, 1,1, 1,0,
-      0,0, 0,1, 1,1, 1,0
-    ])
-
     var cubeVertexBuffer, cubeNormalBuffer, cubeIndexBuffer;
 
-    /**
-    * Initiallize the buffers on the gpu
-    **/
     function init() {
         cubeVertexBuffer = setupStaticArrayBuffer(cubeVertices);
 
+
         cubeNormalBuffer = setupStaticArrayBuffer(cubeNormals);
 
-        cubeIndexBuffer = setUpStaticElementBuffer(cubeIndices);
-
-        cubeTextureBuffer = setupStaticArrayBuffer(cubeTextureCoords);
+        cubeIndexBuffer = setUpStaticElementBuffer(new Uint16Array(cubeIndices));
     }
-
-    /**
-    * This function does the actual rendering. This function can be passed to a
-    * RenderSGNode which takes care of the correct rendering
-    **/
     return function(context) {
         if(cubeVertexBuffer == null) {
             init();
@@ -160,49 +141,126 @@ function cubeRenderer() {
 
         let positionLoc = gl.getAttribLocation(shader, "a_position");
         let normalLoc = gl.getAttribLocation(shader, "a_normal");
-        let texCoordLoc = gl.getAttribLocation(shader, "a_texCoord");
-
 
         setArrayBufferFloat(cubeVertexBuffer, positionLoc, 3);
 
         setArrayBufferFloat(cubeNormalBuffer, normalLoc, 3);
-
-        setArrayBufferFloat(cubeTextureBuffer, texCoordLoc, 2);
 
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeIndexBuffer);
         gl.drawElements(gl.TRIANGLES, cubeIndices.length, gl.UNSIGNED_SHORT, 0); //LINE_STRIP
     }
 }
 
-/**
-* This SGNode is a slight improvement on the SGNode provided by the framework. The
-* RenderSGNode allocates storage space for a new matrix each time the render function
-* is called. This SGNode caches the matrices in a variable whose storage space is reused.
-* Usually this wouldn't really matter, but since the particle system uses quite some storage
-* space, when many allocations happen per frame the browsers garbage collector is called
-* often which kills the applications performance and introduces quite noticable lags.
-*
-* Furthermore this Node disables the vertexAttribArray for the texture coordinates
-* in the shader after rendering. If we wouldn't disable the texture attribute the
-* attribute would remain valid and objects that do not provide texture coordinates
-* wouldn't be draw correctly even if the texture coordinates are never used.
-**/
-class NoAllocRenderSGNode extends RenderSGNode {
+function cylinderRenderer() {
 
-    /**
-    * Constructor that creates the matrices for caching
-    **/
+    var cylinderVertices = new Float32Array([
+      0,0,0
+      ,1.00	,0,0.00
+      ,0.98	,0,0.17
+      ,0.94	,0,0.34
+      ,0.87	,0,0.50
+      ,0.77	,0,0.64
+      ,0.64	,0,0.77
+      ,0.50	,0,0.87
+      ,0.34	,0,0.94
+      ,0.17	,0,0.98
+      ,0.00	,0,1.00
+      ,-0.17,0,	0.98
+      ,-0.34,0,	0.94
+      ,-0.50,0,	0.87
+      ,-0.64,0,	0.77
+      ,-0.77,0,	0.64
+      ,-0.87,0,	0.50
+      ,-0.94,0,	0.34
+      ,-0.98,0,	0.17
+      ,-1.00,0,	0.00
+      ,-0.98,0,	-0.17
+      ,-0.94,0,	-0.34
+      ,-0.87,0,	-0.50
+      ,-0.77,0,	-0.64
+      ,-0.64,0,	-0.77
+      ,-0.50,0,	-0.87
+      ,-0.34,0,	-0.94
+      ,-0.17,0,	-0.98
+      ,0.00	,0,-1.00
+      ,0.17	,0,-0.98
+      ,0.34	,0,-0.94
+      ,0.50	,0,-0.87
+      ,0.64	,0,-0.77
+      ,0.77	,0,-0.64
+      ,0.87	,0,-0.50
+      ,0.94	,0,-0.34
+      , 0.98,0,	-0.17
+
+    ]);
+    var cylinderIndices =  new Uint16Array([
+      ,0,2,1 ,0,3,2 ,0,4,3
+      ,0,5,4 ,0,6,5
+      ,0,7	,6
+      ,0,8	,7
+      ,0,9	,8
+      ,0,10,	9
+      ,0,11,	10
+      ,0,12,	11
+      ,0,13,	12
+      ,0,14,	13
+      ,0,15,	14
+      ,0,16,	15
+      ,0,17,	16
+      ,0,18,	17
+      ,0,19,	18
+      ,0,20,	19
+      ,0,21,	20
+      ,0,22,	21
+      ,0,23,	22
+      ,0,24,	23
+      ,0,25,	24
+      ,0,26,	25
+      ,0,27,	26
+      ,0,28,	27
+      ,0,29,	28
+      ,0,30,	29
+      ,0,31,	30
+      ,0,32,	31
+      ,0,33,	32
+      ,0,34, 33,
+      ,0,35,34,
+      ,0,36,35,
+      ,0,36,1
+
+    ]);
+
+    var cylinderVertexBuffer, cylinderIndexBuffer;
+
+    function init() {
+        cylinderVertexBuffer = setupStaticArrayBuffer(cylinderVertices);
+
+        cylinderIndexBuffer = setUpStaticElementBuffer(cylinderIndices);
+    }
+    return function(context) {
+        if(cylinderVertexBuffer == null) {
+            init();
+        }
+        let shader = context.shader;
+        let gl = context.gl;
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, cylinderVertexBuffer);
+        let positionLoc = gl.getAttribLocation(shader, "a_position");
+        gl.vertexAttribPointer(positionLoc, 3, gl.FLOAT, false,0,0) ;
+        gl.enableVertexAttribArray(positionLoc);
+
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cylinderIndexBuffer);
+        gl.drawElements(gl.TRIANGLES, cylinderIndices.length, gl.UNSIGNED_SHORT, 0);
+    }
+}
+
+class NoAllocRenderSGNode extends RenderSGNode {
     constructor(renderer, children) {
         super(renderer, children)
         this.modelView = mat4.create();
         this.normalMatrix = mat3.create();
     }
 
-    /**
-    * Works similar to setTransformationUniforms of the RenderSGNode but calculates
-    * the matrix multiplications without creating new matrices
-    * @param context - the webgl context
-    **/
     setTransformationUniforms(context) {
         //set matrix uniforms
         mat4.multiply(this.modelView, context.viewMatrix, context.sceneMatrix);
@@ -215,28 +273,16 @@ class NoAllocRenderSGNode extends RenderSGNode {
         gl.uniformMatrix4fv(gl.getUniformLocation(shader, 'u_projection'), false, projectionMatrix);
     }
 
-    /**
-    * Renders underlying object and disables texture coordinates afterwards
-    * @param context - the webgl context
-    **/
     render(context){
         super.render(context);
         gl.disableVertexAttribArray(gl.getAttribLocation(context.shader, "a_texCoord"));
     }
 }
-
 /**
-  * Scenegraph node for transformations that should change with time (they don't have to).
-  * This is done by providing a function to the SGNode with which to recompute the
-  * transformation matrix each frame.
-  **/
+  *Scenegraph node for transformations that should change with time
+  *@param calcMatrixFunc function that takes no parameters and returns a matrix
+  */
 class AnimationSGNode extends SGNode {
-
-    /**
-    * Construcor
-    * @param calcMatrixFunc function that takes no parameters and returns a matrix (mat4)
-    * @param children
-    **/
     constructor(calcMatrixFunc, children) {
         super(children);
         this.calcMatrix = calcMatrixFunc;
@@ -246,7 +292,6 @@ class AnimationSGNode extends SGNode {
     render(context) {
         var previous = context.sceneMatrix;
 
-        //compute the transformation matrix before rendering
         if(previous === null) {
             context.sceneMatrix = this.calcMatrix();
         } else {
@@ -270,7 +315,7 @@ var doorAnimator = function(endTime) {
     var rotate = mat4.create();
     var transForm = mat4.create();
     var animate = false;
-    timeEventQueue.push({timeStamp: 7500, fire: function(){ animate = true}});
+    timeEventQueue.push({timeStamp: 10000, fire: function(){ animate = true}});
 
     resetQueue.push({reset: function() {
         elapsed = 0;
@@ -290,8 +335,7 @@ var doorAnimator = function(endTime) {
         elapsed += timer.delta;
         let t = elapsed / endTime;
         if(t > 1 ) {
-            return transForm;
-            //reverse()
+            reverse()
             t = 0;
         };
         quat.slerp(interpolatedQuat, beginQuat, rotateQuat, t);
@@ -305,77 +349,10 @@ var doorAnimator = function(endTime) {
     }
 }
 
-var genericAnimator = function(startTime,duration,reverseDuration,offset, rotpoint, angles){
-  var endTime = duration;
-  var duration = duration;
-  var reverseDuration = reverseDuration;
-  var elapsed = 0;
-  var isreversing = false;
-  var rotateQuat = quat.create();
-  var beginQuat = quat.create();
-  var interpolatedQuat = quat.create();
-  var inversRotpoint = vec3.negate([], rotpoint);
-  var trans1 = glm.translate(inversRotpoint[0],inversRotpoint[1],inversRotpoint[2]);
-  var trans2 = glm.translate(rotpoint[0],rotpoint[1],rotpoint[2]);
-  var trans3 = glm.translate(offset[0],offset[1],offset[2]);
-  var rotate = mat4.create();
-  var transForm = mat4.create();
-  var animate = false;
-  timeEventQueue.push({timeStamp: startTime, fire: function(){ animate = true}});
-
-  quat.identity(rotateQuat);
-
-  quat.rotateX(rotateQuat,rotateQuat, angles[0]*Math.PI/180);
-  quat.rotateY(rotateQuat,rotateQuat, angles[1]*Math.PI/180);
-  quat.rotateZ(rotateQuat,rotateQuat, angles[2]*Math.PI/180);
-  quat.normalize(rotateQuat,rotateQuat);
-
-  resetQueue.push({reset: function() {
-      elapsed = 0;
-      quat.rotateX(rotateQuat,quat.create(), angles[0]*Math.PI/180);
-      quat.rotateY(rotateQuat,rotateQuat, angles[1]*Math.PI/180);
-      quat.rotateZ(rotateQuat,rotateQuat, angles[2]*Math.PI/180);
-      quat.normalize(rotateQuat,rotateQuat);
-      beginQuat = quat.create();
-      animate = false;
-  }});
-  function reverse() {
-      if(isreversing){
-        endTime = reverseDuration;
-        isreversing = false;
-      }else{
-        endTime = duration;
-        isreversing = true;
-      }
-      const h = beginQuat;
-      beginQuat = rotateQuat;
-      rotateQuat = h;
-      elapsed = 0;
-  };
-
-  return function() {
-      if(! animate)return trans3;
-
-      elapsed += timer.delta;
-      let t = elapsed / endTime;
-      if(t > 1 ) {
-          reverse()
-          t = 0;
-      };
-      quat.slerp(interpolatedQuat, beginQuat, rotateQuat, t);
-      mat4.fromQuat(rotate, interpolatedQuat);
-      mat4.copy(transForm, identityMat);
-      mat4.multiply(transForm, transForm, trans2);
-      mat4.multiply(transForm, transForm, rotate);
-      mat4.multiply(transForm, transForm, trans1);
-      return transForm;
-  }
-}
 var identityMat = mat4.create();
 
 /**
-* returns matrix that performs rotation around points
-**/
+*returns matrix that performs rotation around points*/
 let cachedMat = [];
 let cachedVec = [];
 function rotateAroundPoint(point, rad, rotation){
@@ -388,12 +365,6 @@ function rotateAroundPoint(point, rad, rotation){
     return transForm;
 }
 
-/**
-* convenience function that creates a MaterialSGNode and sets it's material
-* properties to those of the material being passed into the function
-* @param material - material to copy properties from
-* @return a new MaterialSGNode
-**/
 function initMaterialSGNode(material) {
 
     mat = new MaterialSGNode();
@@ -405,14 +376,6 @@ function initMaterialSGNode(material) {
     return mat;
 }
 
-/**
-* Constructor function for a material (call with new to create material)
-* @param ambient - vec4
-* @param diffuse - vec4
-* @param specular - vec4
-* @param emission - vec4
-* @param shininess - float
-**/
 function Material(ambient, diffuse, specular, emission, shininess) {
     this.ambient = ambient;
     this.diffuse = diffuse;
@@ -421,12 +384,6 @@ function Material(ambient, diffuse, specular, emission, shininess) {
     this.shininess = shininess;
 }
 
-/**
-* convenience function that creates a material that is not affected by light
-* sources. This is achieved by setting the materials emission to the color
-* of the parameter and all other material properties tp [0,0,0,1] (black)
-* @param color - vec4 to set the materials emission property to
-**/
 function constantColorMaterial(color) {
     return new Material([0,0,0,1], [0,0,0,1], [0,0,0,1], color, 0);
 }
